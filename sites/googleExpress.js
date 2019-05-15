f5b6fa741733c2a375535f4bc0431070860386b6
Wingysam <wingysam+git@gmail.com>
2019-05-15 12:55:13 -0400

    Google Express requires Puppeteer? No U

--- sites/googleExpress.js
+++ sites/googleExpress.js
@@ -1,16 +1,14 @@
-const puppeteer = require('puppeteer');
+const HttpsProxyAgent = require('https-proxy-agent');
+const fetch = require('node-fetch');
+const cheerio = require('cheerio');
 
 module.exports = async (url, proxy) => {
   const options = {};
-  if (proxy) options.args = [
-    `--proxy-server=${proxy}`
-  ];
-  const browser = await puppeteer.launch(options);
-  const page = await browser.newPage();
-  await page.goto(url);
-  const name = await page.$eval('h1.title', element => {
-    return element.textContent;
-  });
-  await browser.close();
-  return { name };
+  if (proxy) options.agent = new HttpsProxyAgent(require('url').parse(proxy));
+  const res = await fetch(url, options);
+  if (!res.ok) throw new Error(`Res not ok. Status: ${res.status} ${res.statusText}`);
+  const $ = cheerio.load(await res.text());
+  const name = $('h1.title').text();
+  if (name) return { name };
+  throw new Error('Could not find product. Invalid URL?');
 };

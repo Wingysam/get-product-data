9c1dca82ff2e12e5f1a01ba0a6b5435ecbcfc042
Wingysam <wingysam+git@gmail.com>
2019-08-19 10:24:17 -0400

    um ok

--- sitesArray.js
+++ sitesArray.js
@@ -1,13 +1,14 @@
 const path = require('path');
 const fs = require('fs');
 
-const files = fs.readdirSync(path.resolve('./sites'));
+const files = fs.readdirSync(path.join(__dirname, 'sites'));
 
 module.exports = files.map(filename => {
   if (!filename.endsWith('.js')) return;
   return require(
     path.join(
-      path.resolve('./sites'),
+      __dirname,
+      'sites',
       filename
     )
   )

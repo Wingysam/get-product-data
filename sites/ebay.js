34bfdff713cd7f214cff8834f23ec448a8535b2d
Wingy <git@wingysam.xyz>
2020-11-03 16:37:25 -0500

    [eBay] price, image

--- sites/ebay.js
+++ sites/ebay.js
@@ -9,11 +9,15 @@ module.exports = {
   ],
   testCases: [
     {
-      name: 'Nest Learning Thermostat 3rd Generation, Works with Google Home & Amazon Alexa',
+      name: 'Nest T3017US 3rd Generation Programmable Wi-Fi Smart Learning Thermostat - White',
+      price: '$45.65',
+      image: 'https://i.ebayimg.com/images/g/bqgAAOSwPDRfGUt7/s-l640.jpg',
       url: 'https://www.ebay.com/itm/Nest-Learning-Thermostat-3rd-Generation-Works-with-Google-Home-and-Amazon-Alexa/223103414932'
     },
     {
-      name: 'Apple 13.3" MacBook Air 128GB with Retina Display (2018, Silver) MREA2LL/A',
+      name: 'Apple MacBook Air 13.3" (128GB SSD, Intel Core i5 8th Gen., 3.60 GHz, 8GB) Laptop - Silver - MREA2LL/A (October, 2018)',
+      price: '$1,037.25',
+      image: 'https://i.ebayimg.com/images/g/WhIAAOSwTm1eNtQT/s-l640.jpg',
       url: 'https://www.EBAY.com/itm/Apple-13-3-MacBook-Air-128GB-with-Retina-Display-2018-Silver-MREA2LL-A/202501359082'
     }
   ],
@@ -23,8 +27,14 @@ module.exports = {
     const res = await fetch(url, options);
     if (!res.ok) throw new Error(`Res not ok. Status: ${res.status} ${res.statusText}`);
     const $ = cheerio.load(await res.text());
-    const name = $('#itemTitle').text().replace('Details about  ', '');
-    if (name) return { name };
-    throw new Error('Could not find product. Invalid URL?');
+    let name, price, image
+
+    if (!name) name = $('title').text().slice(0, -' for sale online | eBay'.length)
+
+    if (!price) price = $('.display-price').text().trim()
+
+    if (!image) image = $('.app-filmstrip__image').first().attr('src')
+
+    return { name, price, image }
   }
 }

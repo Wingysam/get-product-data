b7b3a546515a9beaf4a855b7a50acbc7203fd2a9
Wingy <git@wingysam.xyz>
2020-11-03 18:00:09 -0500

    [Google Shopping] fix

--- sites/googleShopping.js
+++ sites/googleShopping.js
@@ -14,11 +14,11 @@ module.exports = {
       url: 'https://express.google.com/u/0/product/Google-Chromecast-2nd-Generation-1080p-Wi-Fi-Black/12201320268068482524_12770536777640250214_102760793'
     },
     {
-      name: 'Super Mario Odyssey - Nintendo Switch',
+      name: 'Super Mario Odyssey [Switch Game]',
       url: 'https://express.GOOGLE.com/u/0/product/Nintendo-Super-Mario-Odyssey-Switch-Game/4699429706850701365_8691361685679756397_125181302'
     },
     {
-      name: 'Google Wi-Fi Wireless Router - 2.4 GHz / 5 GHz - Gigabit Ethernet - 802.11b/a/g/n/ac',
+      name: 'Google WiFi AC1200 Dual-Band Mesh Wi-Fi Router - White',
       url: 'https://www.google.com/shopping/product/17811093614708061273?psb=1&tbm=shop&prds=epd%3A1391942677106388859%2Cprmr%3A3&ved=0CAYQ0FUoAWoXChMIj_fj4sPj5QIVmYRaBR0jNAyDECw'
     }
   ],
@@ -28,8 +28,9 @@ module.exports = {
     const res = await fetch(url, options);
     if (!res.ok) throw new Error(`Res not ok. Status: ${res.status} ${res.statusText}`);
     const $ = cheerio.load(await res.text());
-    const name = $('title').text(); // google was nice to me they made the title good
-    if (name) return { name };
-    throw new Error('Could not find product. Invalid URL?');
+
+    const name = $('.fbrNcd').text() // hackery, probably will break but kbye
+
+    return { name }
   }
 }

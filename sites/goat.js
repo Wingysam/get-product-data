e8522c0cc329b06f7a71ec72e230b42c1e7e56f5
Wingy <git@wingysam.xyz>
2020-11-03 17:43:29 -0500

    [Goat] price, image

--- sites/goat.js
+++ sites/goat.js
@@ -10,11 +10,27 @@ module.exports = {
   testCases: [
     {
       name: "Air Jordan 1 Retro High OG 'Crimson Tint'",
+      price: '$149',
+      image: 'https://image.goat.com/1000/attachments/product_template_pictures/images/020/249/059/original/485842_00.png.png',
       url: 'https://www.goat.com/sneakers/air-jordan-1-retro-high-og-crimson-tint-555088-081?_branch_match_id=722603302568876625'
     },
     {
-      name: "Air Jordan 1 Retro High OG 'Shattered Backboard 3.0'",
-      url: 'https://wwW.GOaT.Com/sneakers/air-jordan-1-retro-high-og-shattered-backboard-3-0-555088-028'
+      name: "Air Jordan 1 Retro High OG 'Bloodline'",
+      price: '$150',
+      image: 'https://image.goat.com/1000/attachments/product_template_pictures/images/028/182/432/original/508079_00.png.png',
+      url: 'https://www.GOaT.com/sneakers/air-jordan-1-high-retro-og-bred-2019-555088-062'
+    },
+    {
+      name: "Sk8-Hi 'I Heart Vans'",
+      price: '$70',
+      image: 'https://image.goat.com/1000/attachments/product_template_pictures/images/022/436/551/original/VN0A38GEVP5.png.png',
+      url: 'https://www.goat.com/sneakers/sk8-hi-i-heart-vans-vn0a38gevp5'
+    },
+    {
+      name: 'Sk8-Hi Custom iD',
+      price: 'Sold Out',
+      image: 'https://image.goat.com/1000/attachments/product_template_pictures/images/018/064/320/original/VANS_SK8HI_ID.png.png',
+      url: 'https://www.goat.com/sneakers/sk8-hi-custom-id-vans-sk8hi-id'
     }
   ],
   async getter (url, proxy) {
@@ -26,11 +42,22 @@ module.exports = {
     if (proxy) options.agent = new HttpsProxyAgent(require('url').parse(proxy));
     const res = await fetch(url, options);
     if (!res.ok) throw new Error(`Res not ok. Status: ${res.status} ${res.statusText}`);
-    const $ = cheerio.load(await res.text());
+    const html = await res.text()
+    const $ = cheerio.load(html);
+    let name, price, image
     if ($('title').text() === 'Access to this page has been denied.') throw new Error('Got CAPTCHA')
-    const data = JSON.parse($('script[type="application/ld+json"]').html());
-    const name = data.name
-    if (name) return { name };
-    throw new Error('Could not find product. Invalid URL?');
+    const ldJson = JSON.parse($('script[type="application/ld+json"]').html());
+    const context = JSON.parse(html.split('window.__context__ = ')[1].split('</script>')[0])
+    
+    name = ldJson.name
+
+    if (!price) price = '$' + context.default_store['product-templates'].slug_map[Object.keys(context.default_store['product-templates'].slug_map)[0]].new_lowest_price_cents / 100
+    if (!price) price = $('[data-qa="pdp-out-of-stock-cta-text"]').text().trim()
+    if (!price) price = undefined
+    if (price === '$0') price = 'Sold Out'
+    
+    image = ldJson.image
+
+    return { name, price, image }
   }
 }

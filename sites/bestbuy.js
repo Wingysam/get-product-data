a76890699f6875955ca48c20cee15bf09356ee02
Wingy <git@wingysam.xyz>
2020-11-03 09:13:16 -0500

    [Best Buy] hack to use curl

--- sites/bestbuy.js
+++ sites/bestbuy.js
@@ -1,5 +1,4 @@
-const HttpsProxyAgent = require('https-proxy-agent');
-const fetch = require('node-fetch');
+const { spawn } = require('child-process-promise')
 const cheerio = require('cheerio');
 
 module.exports = {
@@ -10,27 +9,39 @@ module.exports = {
   testCases: [
     {
       name: 'HP - 2-in-1 14" Touch-Screen Chromebook - Intel Core i3 - 8GB Memory - 64GB eMMC Flash Memory - White',
+      price: 'This item is no longer available in new condition.',
+      image: 'https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6301/6301869_sd.jpg',
       url: 'https://www.bestbuy.com/site/hp-2-in-1-14-touch-screen-chromebook-intel-core-i3-8gb-memory-64gb-emmc-flash-memory-hp-finish-in-ceramic-white-and-cloud-blue/6301869.p?skuId=6301869'
     },
     {
-      name: 'Samsung - 75" Class - LED - NU6900 Series - 2160p - Smart - 4K UHD TV with HDR',
+      name: 'Samsung - 75" Class 6 Series LED 4K UHD Smart Tizen TV',
+      price: '$849.99',
+      image: 'https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6290/6290167_sd.jpg',
       url: 'https://www.BESTBUY.com/site/samsung-75-class-led-nu6900-series-2160p-smart-4k-uhd-tv-with-hdr/6290167.p?skuId=6290167'
     }
   ],
   async getter (url, proxy) {
-    const options = {
-      headers: {
-        // Best Buy takes 10x longer without these headers
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:63.0) Gecko/20100101 Firefox/63.0',
-        Connection: 'keep-alive'
-      }
-    };
-    if (proxy) options.agent = new HttpsProxyAgent(require('url').parse(proxy));
-    const res = await fetch(url, options);
-    if (!res.ok) throw new Error(`Res not ok. Status: ${res.status} ${res.statusText}`);
-    const $ = cheerio.load(await res.text());
-    const name = $('.sku-title > h1').text();
-    if (name) return { name };
-    throw new Error('Could not find product. Invalid URL?');
+    // Extreme circumstances call for extreme measures.
+    // All node HTTP libs hang here, but curl doesn't.
+    // I have spent 3 hours debugging, and have given up.
+    // Here be dragons~
+    const { stdout: html } = await spawn('curl', [ url, '-LH', 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:68.0) Gecko/20100101 Firefox/68.0' ], { env: {
+      http_proxy: proxy,
+      https_proxy: proxy
+    }, capture: [
+      'stdout'
+    ] })
+    
+    const $ = cheerio.load(html);
+    let name, price, image
+
+    if (!name) name = $('.sku-title > h1').text().trim()
+
+    if (!price) price = $('.priceView-customer-price').first().children().first().text().trim()
+    if (!price) price = $('.inactive-product-message.v-fw-medium').text().trim()
+
+    if (!image) image = $('meta[property="og:image"]').attr('content')
+
+    return { name, price, image }
   }
 }

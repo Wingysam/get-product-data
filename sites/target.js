3450664835ac7bc03d46f13e4fbbd8ecddb48990
Wingy <git@wingysam.xyz>
2020-11-05 12:51:33 -0500

    [Target] price, image

--- sites/target.js
+++ sites/target.js
@@ -2,6 +2,8 @@ const HttpsProxyAgent = require('https-proxy-agent');
 const fetch = require('node-fetch');
 const cheerio = require('cheerio');
 
+const { og } = require('../util')
+
 module.exports = {
   name: 'Target',
   URLs: [
@@ -10,10 +12,12 @@ module.exports = {
   testCases: [
     {
       name: "Disney Gift Card eGift (Email Delivery)",
+      image: 'https://target.scene7.com/is/image/Target/GUEST_c7c77bed-608c-402d-83c1-0fe945f3157b',
       url: 'https://www.target.com/p/disney-gift-card-egift-email-delivery/-/A-54191074'
     },
     {
       name: 'Xbox One X 1 TB Console - Black',
+      image: 'https://target.scene7.com/is/image/Target/GUEST_0a9f79ff-bf87-4c24-a34d-4a6a9f301309',
       url: 'https://www.TARGET.com/p/xbox-one-x/-/A-52637446'
     }
   ],
@@ -23,8 +27,15 @@ module.exports = {
     const res = await fetch(url, options);
     if (!res.ok) throw new Error(`Res not ok. Status: ${res.status} ${res.statusText}`);
     const $ = cheerio.load(await res.text());
-    const name = $('[data-test="product-title"]').text();
-    if (name) return { name };
-    throw new Error('Could not find product. Invalid URL?');
+    let name, price, image
+
+    name = $('[data-test="product-title"]').text();
+
+    // TODO: implement price
+    price = undefined
+
+    image = 'https:' + og($, 'image')
+
+    return { name, price, image }
   }
 }

68717df0a20fbf9c3d84645e67bb759f9f809521
Wingy <git@wingysam.xyz>
2020-11-05 13:22:01 -0500

    [Walmart] price, image

--- sites/walmart.js
+++ sites/walmart.js
@@ -2,6 +2,8 @@ const HttpsProxyAgent = require('https-proxy-agent');
 const fetch = require('node-fetch');
 const cheerio = require('cheerio');
 
+const { og } = require('../util')
+
 module.exports = {
   name: 'Walmart',
   URLs: [
@@ -10,21 +12,36 @@ module.exports = {
   testCases: [
     {
       name: 'LEGO City Town LEGO® City Advent Calendar 60201',
+      price: '$49.90',
+      image: 'https://i5.walmartimages.com/asr/eb9b5262-1464-4a43-b8d6-3b10afcda5fe_2.2d33be3a3baf104beef79d9c758ba12a.jpeg',
       url: 'https://www.walmart.com/ip/Lego-City-60201-Kid-Children-s-Toy-Set-24-Day-Advent-Calendar-Holiday-Gift-Box/210314543'
     },
     {
       name: 'Nerf Zombie Strike RevReaper Blaster with 10 Zombie Strike Darts',
+      price: '$24.99',
+      image: 'https://i5.walmartimages.com/asr/853e0e83-f7ac-4678-ad76-e9595a51403f_1.f3e87a25fdeed0127613f1eee5a45344.jpeg',
       url: 'https://www.WALMART.com/ip/Nerf-Zombie-Strike-Revreaper/942349359'
     }
   ],
   async getter (url, proxy) {
-    const options = {};
+    const options = {
+      headers: {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:68.0) Gecko/20100101 Firefox/68.0'
+      }
+    };
     if (proxy) options.agent = new HttpsProxyAgent(require('url').parse(proxy));
     const res = await fetch(url, options);
     if (!res.ok) throw new Error(`Res not ok. Status: ${res.status} ${res.statusText}`);
-    const $ = cheerio.load(await res.text());
-    const name = $('h1.prod-ProductTitle').attr().content;
-    if (name) return { name };
-    throw new Error('Could not find product. Invalid URL?');
+    const html = await res.text()
+    const $ = cheerio.load(html);
+    let name, price, image
+
+    name = $('h1.prod-ProductTitle').attr('content')
+
+    price = $('.price > :first-child').first().text()
+
+    image = og($, 'image')
+
+    return { name, price, image }
   }
 }

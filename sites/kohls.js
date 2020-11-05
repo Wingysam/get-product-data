7e0ac2521bf546783961e64dc54f4ba73f746af0
Wingy <git@wingysam.xyz>
2020-11-05 11:05:53 -0500

    [Kohls] price, image

--- sites/kohls.js
+++ sites/kohls.js
@@ -10,10 +10,14 @@ module.exports = {
   testCases: [
     {
       name: 'Fitbit Ace Kids Activity Tracker',
+      price: '29.99',
+      image: 'https://media.kohlsimg.com/is/image/kohls/3296135_Power_Purple',
       url: 'https://www.kohls.com/product/prd-3296135/fitbit-ace-kids-activity-tracker.jsp'
     },
     {
       name: 'Nintendo Switch Console & Joy-Con Controller Set with Bonus Interworks Controller',
+      price: '339.99',
+      image: 'https://media.kohlsimg.com/is/image/kohls/3239600',
       url: 'https://www.KOHLS.com/product/prd-3239600/nintendo-switch-console-joy-con-controller-set-with-bonus-interworks-controller.jsp'
     }
   ],
@@ -22,9 +26,20 @@ module.exports = {
     if (proxy) options.agent = new HttpsProxyAgent(require('url').parse(proxy));
     const res = await fetch(url, options);
     if (!res.ok) throw new Error(`Res not ok. Status: ${res.status} ${res.statusText}`);
-    const $ = cheerio.load(await res.text());
-    const name = $('.pdp-product-title').text();
-    if (name) return { name };
-    throw new Error('Could not find product. Invalid URL?');
+    const html = await res.text()
+    const $ = cheerio.load(html);
+    let jsData = {}
+    try {
+      jsData = JSON.parse(html.split('var productV2JsonData = ')[1].split(';</script>')[0])
+    } catch {}
+    let name, price, image
+
+    name = $('.product-title').text().trim()
+
+    price = $('meta[itemprop="price"]').first().attr('content')
+
+    image = (jsData?.images[0]?.url || '').split('?')[0]
+
+    return { name, price, image }
   }
 }

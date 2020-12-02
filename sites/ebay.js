1ee9e1d28be2096805c259828d4db90df905f1db
Wingy <git@wingysam.xyz>
2020-12-02 12:11:03 -0500

    [eBay] additional type support

--- sites/ebay.js
+++ sites/ebay.js
@@ -4,6 +4,8 @@ const cheerio = require('cheerio')
 const HttpsProxyAgent = require('https-proxy-agent')
 const fetch = require('node-fetch')
 
+const { og } = require('../util')
+
 module.exports = {
   name: 'eBay',
   URLs: [
@@ -13,14 +15,20 @@ module.exports = {
     {
       name: 'Nest T3017US 3rd Generation Programmable Wi-Fi Smart Learning Thermostat - White',
       price: '$45.65',
-      image: 'https://i.ebayimg.com/images/g/bqgAAOSwPDRfGUt7/s-l640.jpg',
+      image: 'https://i.ebayimg.com/images/g/bqgAAOSwPDRfGUt7/s-l600.jpg',
       url: 'https://www.ebay.com/itm/Nest-Learning-Thermostat-3rd-Generation-Works-with-Google-Home-and-Amazon-Alexa/223103414932'
     },
     {
       name: 'Apple MacBook Air 13.3" (128GB SSD, Intel Core i5 8th Gen., 3.60 GHz, 8GB) Laptop - Silver - MREA2LL/A (October, 2018)',
-      price: '$1,037.25',
-      image: 'https://i.ebayimg.com/images/g/WhIAAOSwTm1eNtQT/s-l640.jpg',
+      price: '$949.00',
+      image: 'https://i.ebayimg.com/images/g/WhIAAOSwTm1eNtQT/s-l600.jpg',
       url: 'https://www.EBAY.com/itm/Apple-13-3-MacBook-Air-128GB-with-Retina-Display-2018-Silver-MREA2LL-A/202501359082'
+    },
+    {
+      name: '33FT RGB 600 LED Strip Lights WiFi APP Phone Control Music Sync Fit Alexa Google',
+      price: 'US $21.98/ea',
+      image: 'https://i.ebayimg.com/images/g/vVoAAOSw0LlfVgX3/s-l400.jpg',
+      url: 'https://www.ebay.com/itm/33FT-RGB-600-LED-Strip-Lights-WiFi-APP-Phone-Control-Music-Sync-Fit-Alexa-Google-/233272384608?_trksid=p2349624.m46890.l49292'
     }
   ],
   async getter (url, proxy) {
@@ -31,11 +39,13 @@ module.exports = {
     const $ = cheerio.load(await res.text())
     let name, price, image
 
-    if (!name) name = $('title').text().slice(0, -' for sale online | eBay'.length)
+    if (!name) name = $('h1.product-title').text().trim()
+    if (!name) name = $('meta[name="twitter:title"]').attr('content').trim()
 
     if (!price) price = $('.display-price').text().trim()
+    if (!price) price = $('#prcIsum').text().trim()
 
-    if (!image) image = $('.app-filmstrip__image').first().attr('src')
+    if (!image) image = og($, 'image')
 
     return { name, price, image }
   }

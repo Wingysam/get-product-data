421e3385b72591f4905df284edad304b3d597a14
Wingy <git@wingysam.xyz>
2022-11-24 18:35:31 -0500

    add timeout if getting data hangs

--- index.js
+++ index.js
@@ -2,17 +2,23 @@ const { sitesArray } = require('./util')
 
 const sites = sitesArray()
 
-async function fetchData (getter, url, proxy) {
-  const data = await getter(url, proxy)
+const sleep = time => new Promise(resolve => setTimeout(resolve, time))
+
+async function fetchData (getter, url, proxy, timeout = 10000) {
+  const data = await Promise.race([
+    getter(url, proxy),
+    sleep(timeout)
+  ])
+  if (!data) throw new Error(`Timed out after ${timeout} milliseconds.`)
   if (data.name) data.name = data.name.trim()
   return data
 }
 
-module.exports = async (url, proxy) => {
+module.exports = async (url, proxy, timeout) => {
   for (let i = 0; i < sites.length; i++) {
     for (let j = 0; j < sites[i].URLs.length; j++) {
       if (sites[i].URLs[j].test(url)) {
-        return fetchData(sites[i].getter, url, proxy)
+        return fetchData(sites[i].getter, url, proxy, timeout)
       }
     }
   }
